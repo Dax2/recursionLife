17e393f074e1eda4873834db74378fb9b337dbc9
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-05-08 09:24:40 -0700

    Add files via upload

--- flavorFlave.js
+++ flavorFlave.js
@@ -1,8 +1,10 @@
+// wrapping my head around a simple addition function.
+
 function add(x){
     if (x<=0){
           return 0;
       } else {
-          return x + add (x -1);
+          return x + add (x -1);    //add(3) 6
       }
   }
 
@@ -23,7 +25,18 @@ function flavorFlave(n){
     if (n <=0){
    return 'yeeeeeeeaboiiiiiiii' + 0;
      } else {
-        return  'yo' + n + blaber(n - 1);
+        return  'yo' + n + blaber(n - 1); //flavorFlave(3) "yo3yo2yo1ya0" not sure why im missing "yeaa" part?
     }
+
+}
+
+
+function hurryUp(n){
+    if ( n >= 5){                    //this is my attempt to experiment with diferent base cases.
+        return 'go!';
+    } else {
+        return n + hurryUp(n + 1);   //hurryUp(1) "1234go!"
+    }                                //hurryUp(4) "4go!"
+                                    // hurryUp(6) "go!"    where n is greater than base case 5 we will always return "go!"
 
 }

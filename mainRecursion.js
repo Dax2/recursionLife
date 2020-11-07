78bc3e0ff3cb278f13f55a399299fa7ec63db3ca
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-11-07 14:29:07 -0800

    Update mainRecursion.js

--- mainRecursion.js
+++ mainRecursion.js
@@ -1,3 +1,5 @@
+// some notes from the following resource. https://www.javascripttutorial.net/javascript-recursive-function/
+
 //A recursive function is a function that calls itself until it doesn’t
 //A recursive function always has a condition that stops the function from calling itself.
 

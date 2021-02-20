af0007383124d8ad5062c58e9f95bc21828530a6
Daniel C <41881275+Dax2@users.noreply.github.com>
2021-02-20 00:59:30 -0800

    Update summation.js

--- challenges/summation.js
+++ challenges/summation.js
@@ -1,8 +1,8 @@
 
 var summation = function (num) {
-  let result = 0;
-  for (var i = 1; i <= num; i++) {
-    result += i;
+  let result = 0;                   //storing my result in ver result.
+  for (var i = 1; i <= num; i++) {  //stoping at num. length essentialy 
+    result += i;                //adding the index of i or num and adding it to result.
   }
   
   return result;
